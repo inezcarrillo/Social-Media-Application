0b31856978bc661fb42e038aad4ea43498f000e4
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-04-08 20:33:21 -0500

    created js for Thought models

---
+++ models/Thought.js
@@ -0,0 +1,31 @@
+const { Schema, Types } = require('monogoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId,
+        },
+        thoughtBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        userName: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+builtinModules.exports = thoughtSchema;
