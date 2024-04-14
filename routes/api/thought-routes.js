2e60d6896d1bfdfb46a26c72a6c4f59e3c2bfadf
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-04-13 19:33:31 -0500

    added reaction to routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -6,10 +6,16 @@ const {
     createThought,
     updateThought,
     deleteThought,
+    addReaction,
+    deleteReaction
 } = require('../..controllers/thought-controller');
 
 router.route('/').get(getThought).post(createThought);
 
-router.route('/:id').get(getThoguhtId).put(updateThought).delete(deleteThought);
+router.route('/:id').get(getThoughtId).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reaction').post(addReaction);
+
+router.route('/:thoughtId/reaction/reactionId').delete(deleteReaction);
 
 module.exports = router;
