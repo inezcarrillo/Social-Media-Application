91236d5e2e2ca3b17114a9bad984ec24f870400e
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-05-04 16:49:11 -0500

    corrected existing functions to work

--- server.js
+++ server.js
@@ -9,6 +9,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-const connectionStringURI = `mongodb://127.0.0.1:27017`;
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+});
 
-const client = new MongoClient(connectionStringURI);
+app.post('/thought', (req, res) => {
+    // Use db connection to add a document
+    db.collection('userCollection').insertOne(
+      { name: req.body.name, breed: req.body.breed }
+    )
+      .then(results => res.json(results))
+      .catch(err => {
+        if (err) throw err;
+      });
+  });
+
+  app.get('/user', (req, res) => {
+    // Use db connection to find all documents in collection
+    db.collection('petCollection')
+      .find()
+      .toArray()
+      .then(results => res.json(results))
+      .catch(err => {
+        if (err) throw err;
+      });
+
+    });
