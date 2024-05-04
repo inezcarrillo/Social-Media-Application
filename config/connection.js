c779536fba9b3f5576a6e706fd0becc5e7c1a80f
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-05-04 16:49:36 -0500

    corrected the connections

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-connect('mongodb://127.0.0.1:27017/developersApplications');
+mongoose.connect('mongodb://127.0.0.1:27017/developersApplications');
 
-module.exports = connection;
+module.exports = mongoose.connection;
