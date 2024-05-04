1b0a0967f03a2775943d5ba7af9acceeefb32ff2
Inez Carrillo <inezcarrillo@Inezs-MacBook-Pro.local>
2024-05-04 16:48:47 -0500

    added dateFormate function

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat')
 
 const thoughtSchema = new Schema(
     {
@@ -18,6 +19,7 @@ const thoughtSchema = new Schema(
         createdAt: {
             type: Date,
             default: Date.now,
+            get: timestamp => dateFormat(timestamp)
         },
     },
     {
